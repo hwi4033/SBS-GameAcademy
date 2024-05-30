e350bfd0ef20c45824353e09261f7586d716bb8f
hwi4033 <ckdgnl6381@naver.com>
2024-05-30 15:52:08 +0900

    C# Game

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,13 +1,19 @@
 ﻿namespace Game
 {
-    public class Snake
+    abstract public class Unit
     {
         protected int x;
         protected int y;
+        protected string shape;
     }
-    class SnakeHead : Snake
+    public class Player : Unit
     {
-
+        public Player()
+        {
+            x = 10;
+            y = 10;
+            shape = "◎";
+        }
         public int X
         {
             get { return x; }
@@ -23,40 +29,91 @@
             get { return shape; }
             set { shape = value; }
         }
+        public void Move(int[,] map, ConsoleKeyInfo key)
+        {
+            key = Console.ReadKey();
+
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (map[y - 1, x / 2] != 1) y--;
+                        break;
+                case ConsoleKey.LeftArrow:
+                    if (map[y, x / 2 - 1] != 1) x -= 2;
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (map[y, x / 2 + 1] != 1) x += 2;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (map[y + 1, x / 2] != 1) y++;
+                    break;
+            }
+        }
+    }
+    public class Box : Unit
+    {
+        public Box(int x, int y, string shape)
+        {
+            this.x = x;
+            this.y = y;
+            this.shape = shape;
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public string Shape
+        {
+            get { return shape; }
+            set { shape = value; }
+        }
     }
     internal class Program
     {
-        static void Map()
-        {
-            const int width = 10;
-            const int height = 10;
-
-            int[,] map = new int[width, height];
-
-            if(width == 0 || height == 0) Console.Write("ㅁ")
-        }
         static void Main(string[] args)
         {
-            List <string> snake = new List <string> ();
-            ConsoleKeyInfo key;
-            snake.Add("◎");
+            int[,] map = new int[7, 7] {
+                {0,0,1,1,1,1,0 },
+                {0,0,1,0,0,1,0 },
+                {1,1,1,0,0,1,0 },
+                {1,0,0,0,0,1,1 },
+                {1,0,0,0,0,0,1 },
+                {1,1,1,0,0,0,1 },
+                {0,0,1,1,1,1,1 }
+            };
+            int i, j;           
 
+            Player player = new Player();
+            Box box1 = new Box(5,6,"■");
+
+            player.X = 6;
+            player.Y = 4;
+            player.Shape = "◎";
+            ConsoleKeyInfo key;
+            
             while(true)
             {
-                key = Console.ReadKey();
-                
-                switch(key.Key)
+                for (i = 0; i < map.GetLength(0); i++)
                 {
-                    case ConsoleKey.UpArrow:
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        break;
-                    case ConsoleKey.RightArrow:
-                        break;
-                    case ConsoleKey.DownArrow:
-                        break;
+                    for (j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (map[i, j] == 0) Console.Write("  ");
+                        else if (map[i, j] == 1) Console.Write("▩");
+                    }
+                    Console.WriteLine();
                 }
+
+                Console.SetCursorPosition(player.X, player.Y);
+                Console.Write(player.Shape);
+                Console.Write(box.Shape);
+
+                key = Console.ReadKey();
+                player.Move(map, key);
+
+                Console.Clear();
             }
+            
         }
     }
 }
