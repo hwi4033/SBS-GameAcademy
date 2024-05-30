edb6a8f72abf040bb544335ef59e5fec04eb06a5
hwi4033 <ckdgnl6381@naver.com>
2024-05-30 14:19:44 +0900

    2024.05.30 SOLID 설계 원칙

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,10 +1,62 @@
 ﻿namespace Game
 {
+    public class Snake
+    {
+        protected int x;
+        protected int y;
+    }
+    class SnakeHead : Snake
+    {
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public string Shape
+        {
+            get { return shape; }
+            set { shape = value; }
+        }
+    }
     internal class Program
     {
+        static void Map()
+        {
+            const int width = 10;
+            const int height = 10;
+
+            int[,] map = new int[width, height];
+
+            if(width == 0 || height == 0) Console.Write("ㅁ")
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List <string> snake = new List <string> ();
+            ConsoleKeyInfo key;
+            snake.Add("◎");
+
+            while(true)
+            {
+                key = Console.ReadKey();
+                
+                switch(key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        break;
+                    case ConsoleKey.RightArrow:
+                        break;
+                    case ConsoleKey.DownArrow:
+                        break;
+                }
+            }
         }
     }
 }
